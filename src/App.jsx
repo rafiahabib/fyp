51f963050ffa7890c10766acf3f1edc56ede4cb3
rafiahabib <rafiahabib46@gmail.com>
2025-04-20 00:54:00 +0500

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
-import Homepage from './pages/Homepage'
+
+import { TokenContext } from './Context/TokenContext'
+import Dashboard from './pages/Dashboard'
 function App() {
   return (
     <div>
@@ -11,7 +13,7 @@ function App() {
 
     <Routes>
 
-    <Route path="/" element={token ? <Homepage /> : <Navigate to='/login' />} />
+    <Route path="/" element={token ? <Dashboard /> : <Navigate to='/login' />} />
         <Route path="/login" element={token ? <Navigate to='/' /> : <Login />} />
         <Route path="/signup" element={<Signup />} />
     </Routes>
